20df4b2fcb3bfdbd2325b282ca46bca10113de75
Vyacheslav Knyazev - PC <ks38@mail.ru>
2022-12-28 14:50:48 +0300

    added program

--- task/Program.cs
+++ task/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] array = new string[] { "some word", "one", "Duna", "123", "45678" };
+
+void PrintArray(string[] arr)
+{
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void CheckThreeChars(string[] arr)
+{
+       for (int i = 0; i < arr.Length; i++)
+    {
+        string element = arr[i];
+        if (element.Length <= 3)
+        {
+            arr[i] = element;
+        }
+        else arr[i] =  string.Empty;
+
+    }
+}
+
+PrintArray(array);
+CheckThreeChars(array);
+Console.Write("-> ");
+PrintArray(array);
